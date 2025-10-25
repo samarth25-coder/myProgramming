690be701e2030efa4c047a46bec715a2303e0c5a
Samarth Mishra <samarthmishra7753@gmail.com>
2025-10-25 15:16:11 +0530

    :

---
+++ Py_Know/Oct_25th_Sat_Array/addition_of_two_no.py
@@ -0,0 +1,21 @@
+
+
+
+# 25th Oct 2025 (Satursday) learnArray
+Use int() → for whole numbers
+
+Use float() → for decimal numbers
+
+# Ques. 1 Write a program to input 2 numbers and print their sum
+num1 = 5
+num2 = 11
+Sum = num1 + num2
+print(Sum)
+
+
+
+# solultion
+num1 = float(input("Enter your first number: "))
+num2 = float(input("Enter your second number: "))
+Sum = num1 + num2
+print("The sum of two numbers =", Sum)
