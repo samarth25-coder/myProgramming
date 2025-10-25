918228a5a14fde3e34dd9465dbda33ff096bc916
Samarth Mishra <samarthmishra7753@gmail.com>
2025-10-25 15:57:43 +0530

    WAP of input of 2 floating point number and print their average

--- Py_Know/Oct_25th_Sat_Array/WAP_area of square.py
+++ Py_Know/Oct_25th_Sat_Array/WAP_area of square.py
@@ -8,3 +8,13 @@
 side = float(input("Enter the side length in cm :"))
 area = side * side
 print(area)
+
+# 1.5 ques - 1.5 WAP to input two floating point number and print their average
+
+num1 = float(input("Enter your first no :"))
+num2 = float(input("Enter second first no :"))
+
+# Hmne phle sum kiya hai fir jakr hmne average formula use kiya
+Sum = num1 + num2 
+mean = Sum/2
+print(mean)
